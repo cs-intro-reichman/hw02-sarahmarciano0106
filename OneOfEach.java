c5dcc835ac3cb99a9b8ecfde3f8f424bf8de592e
sarahmarciano0106 <154500367+sarahmarciano0106@users.noreply.github.com>
2024-01-04 20:05:57 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,4 +5,26 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
+		boolean g = false;
+		boolean b = false;
+		int count = 0;
+
+		while ( g == false || b == false) {
+		 	if ( (Math.random() ) < 0.5 ){
+		 		g = true;
+		 		System.out.print( "g ");
+		 	}
+		 	else {
+		 		b = true;
+		 		System.out.print( "b ");
+		 	}
+		 	count ++;		 		
+		 }
+
+		System.out.println( " You made it... and you now have " + count + "children.");
+
+
+
+	}
+}
 		
